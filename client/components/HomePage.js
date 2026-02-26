0f2d0a5f290217c5eff6fda7a04fb5334f8a9ee9
Akinwumi Olumide Emmanuel <akinwumiolumide5@gmail.com>
2026-02-26 15:42:40 +0100

    feat: enable ephemeral session for OAuth authentication in HomePage

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -340,6 +340,7 @@ export default function HomePage({
         const result = await WebBrowser.openAuthSessionAsync(
           data.authUrl,
           "crosspost://",
+          { preferEphemeralSession: true },
         );
 
         setModalVisible(false);
